e384c69d98ce4803470ab77efbf14253f999cecd
Aleksey-Smouzh <xmz@tut.by>
2022-01-02 19:16:26 +0300

    layout 02.01.22

--- recipes/script.js
+++ recipes/script.js
@@ -1,31 +1,48 @@
-let keyArray = [];
-function urlCreation() {
-  url = "https://api.edamam.com/api/recipes/v2?type=public";
-  keyArray.forEach((element) => {
-    url += `&${element.name}=${element.value}`;
-  });
-}
-let url = "https://api.edamam.com/api/recipes/v2?type=public";
-let promise;
-let submit = document.getElementById("submit");
-let q;
-submit.addEventListener("click", () => {
-  if (document.querySelector(".search").value !== "") {
-    q = document.querySelector(".search").value;
+
+
+fetch(
+  "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=8d922c31&app_key=af905ade37b309ff4539c4999530672a"
+)
+  .then(function (resp) {
+    return resp.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    document.querySelector(".recipe_name").innerHTML =
+      data.hits[0].recipe.label;
+    document.querySelector(".recipt_link").innerHTML = data.hits[0].recipe.url;
+    
+    // const arrayIngredientLines = [data.hits[0].recipe.ingredientLines];
+    // for (let i = 0; i < arrayIngredientLines.length; i++) {
+    //     const element = arrayIngredientLines[i];
+    //    let columnIngredientLi = document.createElement('li')
+    //    columnIngredientLi.innerHTML = element;
+    // }
+   
+
+     document.querySelector(".ingredients_recipe_list").innerHTML =
+      data.hits[0].recipe.ingredientLines;
+
+
+    document.querySelector(".calories").innerHTML =
+      Math.round(data.hits[0].recipe.calories) +
+      " " +
+      "total" +
+      " " +
+      "calories.";
+    document.querySelector(
+      ".foto_dish"
+    ).innerHTML = `<img src="${data.hits[0].recipe["image"]}" alt="foto_dish"/>`;
+  function f1 () {
+let but = document.querySelector('.totalNutrientsd').innerHTML = data.hits[0].recipe.totalNutrients;
+
   }
-  keyArray = [
-    { name: "q", value: q },
-    { name: "app_id", value: "8d922c31" },
-    { name: "app_key", value: "af905ade37b309ff4539c4999530672a" },
-  ];
-  urlCreation();
-  promise = fetch(url);
-  promise
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    });
-  document.querySelector(".search").value = "";
-});
+document.querySelector(".totalNutrientsd").onclick = f1;
+
+
+
+
+  });
+
+//ingredientLines
+//data.hits[0].recipe.image
