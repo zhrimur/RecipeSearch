82aa3304923b99ba3193917b8db180e0d2702cca
Aleksey-Smouzh <xmz@tut.by>
2022-01-06 00:14:20 +0300

    merg 06.01.22

--- recipes/script.js
+++ recipes/script.js
@@ -1,3 +1,51 @@
+//_____________________________________________________________Алексей
+
+// fetch(
+//   "https://api.edamam.com/api/recipes/v2?type=public&q=chicken&app_id=8d922c31&app_key=af905ade37b309ff4539c4999530672a"
+// )
+//   .then(function (resp) {
+//     return resp.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//     document.querySelector(".recipe_name").innerHTML =
+//       data.hits[0].recipe.label;
+//     document.querySelector(".recipt_link").innerHTML = data.hits[0].recipe.url;
+    
+//     // const arrayIngredientLines = [data.hits[0].recipe.ingredientLines];
+//     // for (let i = 0; i < arrayIngredientLines.length; i++) {
+//     //     const element = arrayIngredientLines[i];
+//     //    let columnIngredientLi = document.createElement('li')
+//     //    columnIngredientLi.innerHTML = element;
+//     // }
+   
+
+//      document.querySelector(".ingredients_recipe_list").innerHTML =
+//       data.hits[0].recipe.ingredientLines;
+
+
+//     document.querySelector(".calories").innerHTML =
+//       Math.round(data.hits[0].recipe.calories) +
+//       " " +
+//       "total" +
+//       " " +
+//       "calories.";
+//     document.querySelector(
+//       ".foto_dish"
+//     ).innerHTML = `<img src="${data.hits[0].recipe["image"]}" alt="foto_dish"/>`;
+//   function f1 () {
+// let but = document.querySelector('.totalNutrientsd').innerHTML = data.hits[0].recipe.totalNutrients;
+
+//   }
+// document.querySelector(".totalNutrientsd").onclick = f1;
+
+
+
+
+//   });
+
+//ingredientLines
+//data.hits[0].recipe.image
 let keyArray = [];
 let filtArray = [];
 //=== обработка фильтра===
@@ -24,8 +72,8 @@ let promise;
 let submit = document.getElementById("submit");
 let q;
 submit.addEventListener("click", () => {
-  if (document.querySelector(".search").value !== "") {
-    q = document.querySelector(".search").value;
+  if (document.querySelector(".search_field").value !== "") {
+    q = document.querySelector(".search_field").value;
   }
   keyArray = [
     { name: "q", value: q },
@@ -54,7 +102,7 @@ submit.addEventListener("click", () => {
     .then((data) => {
       console.log(data);
     });
-  document.querySelector(".search").value = "";
+  document.querySelector(".search_field").value = "";
   filtArray = [];
   keyArray = [
     { name: "q", value: q },
